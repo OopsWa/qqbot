e39b1fceb31f9e626f54bbd9d03a3ea368cb5925
OopsWa <1002901669@qq.com>
2016-09-22 01:34:35 +0900

    1234

---
+++ chat/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOSTNAME: 'www.tuling123.com',
+    PATH: '/openapi/api',
+    PORT: '80',
+    APIkey: '05f082c6693e40e887c8f9ca01bee344',
+    UserId: 'qqbot',
+    keyWords: {
+        superString: ["呀"],
+        startString: ["j"],
+    },
+    RandomAnswer: ["好厉害"
+    ]
+};
